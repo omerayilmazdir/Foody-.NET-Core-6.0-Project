61ea513cd9ccc1a600626ab9a07799caad4d8356
Ömer Ayılmazdır <omeraylmazdr@gmail.com>
2024-09-12 17:36:25 +0300

    DataAccessLayer created.

---
+++ Foody.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foody.DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class // burada T değerinin sınıf olması gerektiğini beyan ediyoruz
+    {
+        // şimdi buraya yöntemlerimizi yani metotlarımızı yazıyoruz
+
+        void Insert(T entity);
+
+        void Update(T entity);
+
+        void Delete(int id);
+
+        List<T> GetAll();
+
+        T GetById(int id);
+    }
+}
