955f14869e3fe315b64e40ef20ba5a4233bcc2be
Ömer Ayılmazdır <omeraylmazdr@gmail.com>
2024-10-15 20:47:55 +0300

    Admin Template layout page

---
+++ Foody.PresentationLayer/ViewComponents/AdminLayoutViewComponents/_HeadLayoutComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Foody.PresentationLayer.ViewComponents.AdminLayoutViewComponents
+{
+	// viewcomponent sınıfından miras alıyoruz
+	public class _HeadLayoutComponentPartial : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
